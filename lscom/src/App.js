427e33ce817d80f9143a5e595c6c74a5b2b40604
Alphaxius <jess@lasershaft.com>
2022-11-09 17:19:32 -0800

    Added base reactjs app for temporary web presence

---
+++ lscom/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Lasershaft.com
+      </header>
+      <p className="App-subheader">
+        Coming soon to own on DVD and video.
+      </p>
+      <div className="box-body"><p className="box-body-text">WOW</p></div>
+    </div>
+  );
+}
+
+export default App;
