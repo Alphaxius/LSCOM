ab50d4b2b890e51a975d71309b5e7f0112163f13
Jess Laser Beam <jess@lasershaft.com>
2023-01-07 16:31:07 -0800

    new blog post; also updated some more css while i was at it

--- lscom/src/blog/blogReader.js
+++ lscom/src/blog/blogReader.js
@@ -60,8 +60,8 @@ const interpretHtml = (content) => {
 						key={keyMaker()}
 						title={splitInnerValues[1]}
 						src={"https://drive.google.com/file/d/"+splitInnerValues[0]+"/preview"}
-						width="100%"
-						height="600vh"
+						width="90%"
+						height="800vmin"
 					>
 					</iframe>
 				);
