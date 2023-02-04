49a929b65339dd5982330113b6a62b510754543e
Jesella <jess@lasershaft.com>
2023-02-04 11:50:11 -0800

    updated blog post workflow python program now should automate generation of index file and sitemap

--- update_sitemap.py
+++ update_sitemap.py
@@ -1,24 +1,30 @@
 import json
-
-def read_text_file(filename):
+from random import randint
+import sys
+import os.path
+import xml.etree.ElementTree as ET
+			
+### read newblog.txt and parse into json
+def read_text_file():
+	uri = ""
 	title = ""
 	date = ""
 	year = ""
 	sections = []
 	tags = []
-	#headers = []
-	#content = []
-	with open(filename, "r") as formatthis:
+	with open("newblog.txt", "r") as formatthis:
 		linedex = 0
 		state = 0
 		for line in formatthis.readlines():
 			if linedex == 0:
-				title = line.strip()
+				uri = line.strip()
 			elif linedex == 1:
-				year = line.strip()
+				title = line.strip()
 			elif linedex == 2:
-				date = line.strip()
+				year = line.strip()
 			elif linedex == 3:
+				date = line.strip()
+			elif linedex == 4:
 				tags = line.strip().split()
 			elif line.strip() == "":
 				state = 1
@@ -41,36 +47,106 @@ def read_text_file(filename):
 			"date": date,
 			"sections": sections
 		}
-	return (output, year, tags)
+	return (output, year, uri, tags)
 		
-		
+### load blogIndex.json into a python object
 def read_index_file():
-	next_index = 0
+	indexdata = []
 	with open("./lscom/src/blog/blogIndex.json", 'r') as indexfile:
 		indexdata = json.load(indexfile)
-		next_index = [datum["index"] for datum in indexdata if "index" in datum.keys()]
-		
+	return indexdata
 
-def convert_to_json(filename):
-	(output, year, tags) = read_text_file("./lscom/src/blog/"+filename)
-	
-	with open("testfile.json", 'w') as writeto:
-		json.dump(output, writeto, indent = 2)
-		
-	
+### return a random character string
+def create_file_name():
+	return "".join([chr(randint(65,90)) for _ in range(16)])
+
+### return file name and file location for new blog post
+def file_name_loc_wrap():
+	filename = create_file_name()
+	return (filename, "./lscom/src/blog/"+filename+".json")
+
+### regenerate sitemap.xml
+def convert_to_xml(year, uri):
+	ET.register_namespace("","http://www.sitemaps.org/schemas/sitemap/0.9")
+	sitemap = ET.parse("./lscom/public/sitemap.xml")
+	sitemaproot = sitemap.getroot()
+	newxmllocpre = "<url><loc>https://www.lasershaft.com/Blog/"
+	newxmllocpst = "</loc></url>"
+	newxml = ET.fromstring(newxmllocpre+year+"/"+uri+newxmllocpst)
+	sitemaproot.append(newxml)
+	return sitemaproot
+
+### generates blog file json and blog index json
+def convert_to_json():
+	(output, year, uri, tags) = read_text_file()
+	(filename, fileloc) = file_name_loc_wrap()
+	while os.path.isfile(fileloc):
+		(filename, fileloc) = file_name_loc_wrap()
+
 	indexoutput = {
 		"year": year,
-		"name": title,
-		"file": filename.split('.')[0],
+		"name": uri,
+		"file": filename,
 		"index": 0,
 		"prev": "",
 		"next": "",
 		"tags": tags
 	}
-	with open("testindexfile.json", 'w') as indexfile:
-		json.dump({"year": year})
+
+	indexdata = read_index_file()
+	currentindex = indexdata[0]["gotoindex"]
+	currentyear = indexdata[0]["gotoyear"]
+
+	while year != currentyear:
+		changeyear = str(input("change current year from "+currentyear+" to "+year+"? (abort/Y/N) ")).strip()
+		if changeyear == "Y":
+			indexdata[0]["gotoyear"] = year
+			currentyear = year
+			break
+		elif changeyear == "N":
+			break
+		else:
+			print("Aborted, current year did not match and user did not input Y or N")
+			sys.exit(1)
+  
+	newindex = (((currentindex) + 10) // 10) * 10
+	indexoutput["index"] = newindex
+	indexdata[0]["gotoindex"] = newindex
+	
+	prevdatumdex = 0
+	prevdatum = {}
+	for (datumdex, datum) in enumerate(indexdata):
+		if not("index" in datum.keys()):
+			continue
+		if datum["index"] == currentindex:
+			prevdatumdex = datumdex
+			prevdatum = datum
+			break
+			
+	newsubpage = "/"+year+"/"+uri
+	
+	indexoutput["prev"] = "/"+prevdatum["year"]+"/"+prevdatum["name"]
+	indexoutput["next"] = newsubpage
+	
+	indexdata[prevdatumdex]["next"] = newsubpage
+	indexdata.insert(1, indexoutput)
+	
+	return (fileloc, output, indexdata)
+
+	
+def main():
+	(blogfileloc, blogjson, blogindexjson) = convert_to_json()
+	sitemapxml = convert_to_xml(blogindexjson[1]["year"], blogindexjson[1]["name"])
+	with open(blogfileloc, 'w') as newblogfile:
+		json.dump(blogjson, newblogfile, indent = 2)
+	with open("./lscom/src/blog/blogIndex.json", "w") as blogindexfile:
+		json.dump(blogindexjson, blogindexfile, indent = 2)
+	with open("./lscom/public/sitemap.xml", 'w') as sitemapfile:
+		sitemapfile.write(ET.tostring(sitemapxml, xml_declaration=True, encoding="UTF-8"))
 	
 	
+if __name__ == "__main__":
+	main()
 	
 	
 	
